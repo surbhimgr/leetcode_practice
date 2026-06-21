55f607178ecf62b1d257bbe5385eda9d93776d02
Surbhi Suman <45829819+surbhimgr@users.noreply.github.com>
2026-06-21 22:05:23 +0530

    Implement logic to count song pairs divisible by 60
    Added a solution outline for counting pairs of songs with total durations divisible by 60.

--- Pairs_of_Songs_With_Total_Durations_Divisible_by_60.py
+++ Pairs_of_Songs_With_Total_Durations_Divisible_by_60.py
@@ -1,5 +1,8 @@
 '''You are given a list of songs where the ith song has a duration of time[i] seconds.
-Return the number of pairs of songs for which their total duration in seconds is divisible by 60. Formally, we want the number of indices i, j such that i < j with (time[i] + time[j]) % 60 == 0.'''
+Return the number of pairs of songs for which their total duration in seconds is divisible by 60. Formally, we want the number of indices i, j such that i < j with (time[i] + time[j]) % 60 == 0.
+
+Solution -maintain a dict and parse the list, for a value i we calculate the remainder when divided by 60, for bieng divisible by 60 we need to find if (60-remainder)%60 exists in the dict.
+'''
 
 class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
