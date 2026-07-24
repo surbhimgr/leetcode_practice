624eccc8954ef9f364e77335d2cf8d86581ed425
Surbhi Suman <45829819+surbhimgr@users.noreply.github.com>
2026-07-24 22:37:22 +0530

    Enhance Maximum Points problem statement and solution
    Added problem description and solution outline for maximum points from cards.

---
+++ Maximum Points You Can Obtain from Cards.py
@@ -0,0 +1,24 @@
+''' 
+There are several cards arranged in a row, and each card has an associated number of points. The points are given in the integer array cardPoints.
+
+In one step, you can take one card from the beginning or from the end of the row. You have to take exactly k cards.
+
+Your score is the sum of the points of the cards you have taken.
+
+Given the integer array cardPoints and the integer k, return the maximum score you can obtain. 
+
+Solution- find the window for which sum is minimum, then substract it from total sum.
+'''
+
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        n=len(cardPoints)
+        j=n-k
+        cpsum = sum(cardPoints)
+        winsum = sum(cardPoints[:j])
+        minwid = winsum
+        for i in range(j,n):
+            winsum += cardPoints[i]-cardPoints[i-j]
+            minwid=min(minwid,winsum)
+        return cpsum-minwid
+        
